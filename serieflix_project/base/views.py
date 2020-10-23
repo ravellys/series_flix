a00c9751647899cdf5d004af973bb0be76de7454
ravellys <lucas.ravellys@ufpe.br>
2020-10-23 06:31:21 -0300

    criação da pag inicial

--- serieflix_project/base/views.py
+++ serieflix_project/base/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Ola Mundo')
+    return render(request, 'base/home.html')
