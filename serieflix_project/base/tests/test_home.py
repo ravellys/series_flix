a46d74f23fc830fddce4cfd41c216be9061cc58f
ravellys <lucas.ravellys@ufpe.br>
2020-10-23 23:50:26 -0300

    criação do app d gnro

--- serieflix_project/base/tests/test_home.py
+++ serieflix_project/base/tests/test_home.py
@@ -25,4 +25,3 @@ def test_title_link(resp):
 
 def test_footer_link(resp):
     assert_contains(resp, "mailto:lucas.ravellys@ufpe.br")
-
--- serieflix_project/genero/tests/test_genero.py
+++ serieflix_project/genero/tests/test_genero.py
@@ -2,7 +2,7 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
-from serieflix_project.django_assertions import assert_contains
+# from serieflix_project.django_assertions import assert_contains
 
 
 @pytest.fixture
@@ -13,4 +13,3 @@ def resp(client: Client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
-
